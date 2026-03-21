451b34c2703bb6512112c5f887d2615c66a52cf7
josep <josep_1000@hotmail.com>
2026-03-21 20:14:27 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+// import AppRoutes from "./routes/AppRoutes";
+// import { BrowserRouter } from "react-router-dom"
+
+function App() {
+  
+  return (
+      // <BrowserRouter>
+      //   <main>
+      //     <AppRoutes />
+      //   </main>
+      // </BrowserRouter>
+      <main></main>
+  )
+}
+
+export default App
